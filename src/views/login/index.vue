8512525d2dea7baf6b88392a55ea1443e549da22
wolaishishihuo <2430250298@qq.com>
2025-12-17 14:27:10 +0800

    feat(ui): add shadcn-vue components and update dependencies

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-
+import LoginForm from '@/views/login/components/LoginForm.vue'
 </script>
 
 <template>
-  <div>
-    login
+  <div class="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+    <div class="w-full max-w-sm">
+      <LoginForm />
+    </div>
   </div>
 </template>
